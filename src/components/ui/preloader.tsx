6202c8444e01335d013bea0bee3bbfb6f13b27a3
Kennybabs-py <babalolakehinde74@gmail.com>
2024-10-19 11:42:43 +0100

    updated animation

--- src/components/ui/preloader.tsx
+++ src/components/ui/preloader.tsx
@@ -1,13 +1,10 @@
 "use client";
-import { gsap, useGSAP, SplitType } from "@/utils/gsap";
+import { gsap, useGSAP } from "@/utils/gsap";
 export default function Preloader() {
   useGSAP(() => {
-    window.addEventListener("DOMContentLoaded", () => {});
     const content = document.querySelector(".content")! as HTMLDivElement;
-    const preloaderText = new SplitType(".preloader__text", {
-      types: "words",
-    });
-    const preloaderNumber = document.querySelector(".preloader__number")!;
+
+    const preloaderNumber = document.querySelector(".preloader__number > p")!;
 
     let count = 0;
     const images = document.querySelectorAll("img");
@@ -18,72 +15,93 @@ export default function Preloader() {
       preloaderNumber.innerHTML = `${Math.round(percent * 100)}%`;
 
       if (count === images.length) {
-        onLoaded();
+        setTimeout(() => {
+          gsap.fromTo(
+            preloaderNumber,
+            {
+              y: "0",
+              skewY: 0,
+              autoAlpha: 1,
+            },
+            {
+              y: "-200",
+              skewY: 5,
+              delay: 1,
+              autoAlpha: 0,
+              duration: 1.5,
+              ease: "power1.inOut",
+              onComplete: () => animateOut(),
+            }
+          );
+        }, -500);
       }
     }
+
+    gsap.to(preloaderNumber, {
+      y: 0,
+      duration: 1,
+      ease: "power3.inOut",
+      delay: 1,
+      onComplete: () => load(),
+    });
+
+    function animateOut() {
+      gsap.fromTo(
+        ".preloader__number",
+        {
+          autoAlpha: 1,
+        },
+        {
+          autoAlpha: 0,
+          duration: 1,
+          ease: "power1.inOut",
+        }
+      );
+      gsap.to(".preloader__text > span", {
+        // y: "300",
+        // skewY: 5,
+        autoAlpha: 0,
+        duration: 1.5,
+        ease: "power1.inOut",
+        stagger: 0.1,
+        onComplete: () => {
+          gsap.to(".preloader", {
+            clipPath: "polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)",
+            delay: 0.5,
+            duration: 2,
+            ease: "expo.out",
+            onComplete: () => {
+              gsap.to(".preloader", {
+                scaleY: 0,
+                transformOrigin: "100% 100%",
+                duration: 0.5,
+                ease: "expo.out",
+              });
+            },
+          });
+          content?.style.setProperty("opacity", "1");
+          content?.style.setProperty("height", "auto");
+          content?.style.setProperty("overflow", "unset");
+        },
+      });
+    }
+
     function load() {
       images.forEach(() => {
         onImagesLoaded();
       });
     }
-
-    function onLoaded() {
-      gsap.fromTo(
-        preloaderNumber,
-        {
-          x: "0",
-          skewY: 0,
-          autoAlpha: 1,
-        },
-        {
-          x: "200",
-          skewY: -5,
-          autoAlpha: 1,
-          duration: 1.5,
-          ease: "power1.inOut",
-          onComplete: () => {
-            preloaderText.words?.forEach((word) => {
-              gsap.fromTo(
-                word,
-                {
-                  y: "0",
-                  skewY: 0,
-                  autoAlpha: 1,
-                },
-                {
-                  y: "100",
-                  skewY: 5,
-                  autoAlpha: 1,
-                  duration: 1.5,
-                  ease: "power1.inOut",
-                  stagger: 0.1,
-                  onComplete: () => {
-                    gsap.to(".preloader", {
-                      scaleY: 0,
-                      transformOrigin: "100% 100%",
-                      duration: 2,
-                      ease: "expo.out",
-                      onComplete: () => {},
-                    });
-                    content?.style.setProperty("opacity", "1");
-                    content?.style.setProperty("height", "auto");
-                    content?.style.setProperty("overflow", "unset");
-                  },
-                }
-              );
-            });
-          },
-        }
-      );
-    }
-    load();
   }, [".preloader"]);
   return (
     <div className="preloader">
       {/* <div className="preloader__line left" />
       <div className="preloader__line right" /> */}
-      <h3 className="preloader__text">Oliver&apos;s and Matthews</h3>
-      <h3 className="preloader__number">0%</h3>
+      <h3 className="preloader__text">
+        <span>Oliver&apos;s </span> <span>and</span> <span>Matthews</span>
+      </h3>
+      <h3 className="preloader__number">
+        <p>0%</p>
+      </h3>
     </div>
   );
 }
