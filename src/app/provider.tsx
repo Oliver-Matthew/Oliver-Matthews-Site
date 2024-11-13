07ba26ced38e72b7edf18a26f53388b799d2c9b9
Aliu Salaudeen <ayokunumi99@gmail.com>
2024-11-14 00:07:09 +0100

    update contact email

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,6 +1,12 @@
 "use client";
 
-import { Fragment, ReactNode, useCallback, useEffect, useState } from "react";
+import {
+	Fragment,
+	type ReactNode,
+	useCallback,
+	useEffect,
+	useState,
+} from "react";
 import { usePathname } from "next/navigation";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
@@ -10,45 +16,45 @@ import Layout from "@/components/layout";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 type Props = {
-  children: ReactNode;
+	children: ReactNode;
 };
 
 export default function AppProvider({ children }: Props) {
-  const pathname = usePathname();
-  const [template, setTemplate] = useState<string | null>(null);
+	const pathname = usePathname();
+	const [template, setTemplate] = useState<string | null>(null);
 
-  const getTemplate = useCallback(() => {
-    const _pathname = pathname.replace("/", "");
-    if (_pathname === "") {
-      setTemplate("home");
-      return;
-    }
-    if (_pathname === "about-us") {
-      setTemplate("about");
-      return;
-    }
-    if (_pathname === "careers") {
-      setTemplate("careers");
-      return;
-    }
-    setTemplate(pathname);
-    return;
-  }, [pathname]);
+	const getTemplate = useCallback(() => {
+		const _pathname = pathname.replace("/", "");
+		if (_pathname === "") {
+			setTemplate("home");
+			return;
+		}
+		if (_pathname === "about-us") {
+			setTemplate("about");
+			return;
+		}
+		if (_pathname === "careers") {
+			setTemplate("careers");
+			return;
+		}
+		setTemplate(pathname);
+		return;
+	}, [pathname]);
 
-  useEffect(() => {
-    getTemplate();
-  }, [getTemplate]);
+	useEffect(() => {
+		getTemplate();
+	}, [getTemplate]);
 
-  useGSAP(() => {
-    new Interactions({ template: template });
-    window.scrollTo(0, 0);
-  }, [template]);
+	useGSAP(() => {
+		new Interactions({ template: template });
+		window.scrollTo(0, 0);
+	}, [template]);
 
-  gsap.registerPlugin(ScrollTrigger);
+	gsap.registerPlugin(ScrollTrigger);
 
-  return (
-    <Fragment>
-      <Layout template={template}>{children}</Layout>
-    </Fragment>
-  );
+	return (
+		<Fragment>
+			<Layout template={template}>{children}</Layout>
+		</Fragment>
+	);
 }
--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,23 +1,23 @@
 "use client";
 
-import { Fragment, ReactNode } from "react";
+import { Fragment, type ReactNode } from "react";
 
 import Header from "./header";
 import Footer from "./footer";
 import Preloader from "../ui/preloader";
 type Props = {
-  children: ReactNode;
-  template: string | null;
+	children: ReactNode;
+	template: string | null;
 };
 export default function Layout({ children, template }: Props) {
-  return (
-    <Fragment>
-      <Preloader />
-      <div className="content" id="content" data-template={template}>
-        <Header />
-        {children}
-        <Footer />
-      </div>
-    </Fragment>
-  );
+	return (
+		<Fragment>
+			<Preloader />
+			<div className="content" id="content" data-template={template}>
+				<Header />
+				{children}
+				<Footer />
+			</div>
+		</Fragment>
+	);
 }
