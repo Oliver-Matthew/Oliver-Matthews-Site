c7c05c72d69e450d488c4ed97bfa386eec3b7e2f
goons <daramola76@gmail.com>
2024-11-19 10:24:01 +0100

    Blog
    Blog

--- src/app/blog-post/[blogId]/page.tsx
+++ src/app/blog-post/[blogId]/page.tsx
@@ -1,53 +1,32 @@
 import { query } from "../../../lib/hashnode";
 import Image from "next/image";
 
-interface PostParams {
-  post: {
-    title: string;
-    content: {
-      html: string;
-    };
-    coverImage: {
-      url: string;
+interface Post {
+  author: {
+    name: string;
+    profilePicture: string;
+    socialMediaLinks: {
+      twitter: string;
     };
   };
+  content: {
+    html: string;
+  };
+  coverImage: {
+    url: string;
+  };
+  id: string;
+  publishedAt: string;
+  title: string;
 }
 
-interface PageProps {
-  post: PostParams['post'];
+interface PostParams {
+  params: { blogId: string };
 }
 
-export default function BlogPost({ post }: PageProps) {
-  if (!post) {
-    return <div>Post not found</div>;
-  }
+export default async function BlogPost({ params }: PostParams) {
+  const { blogId } = params;
 
-  return (
-    <main className="change__service__page">
-      <figure className="change__service__image">
-        <Image
-          src={post.coverImage.url}
-          alt="Post Cover Image"
-          width={1440}
-          height={400}
-          layout="responsive"
-          objectFit="cover"
-        />
-      </figure>
-
-      <h2 className="change__service__page__header">{post.title}</h2>
-
-      <div className="change__service__content__blog">
-        <div dangerouslySetInnerHTML={{ __html: post.content.html }} />
-      </div>
-    </main>
-  );
-}
-
-export async function getServerSideProps({ params }: { params: { blogId: string } }) {
-  const blogId = params.blogId;
-
-  // Fetch data from the GraphQL API
   const { data: { post } } = await query({
     query: `
       query Post($id: ID!) {
@@ -72,9 +51,29 @@ export async function getServerSideProps({ params }: { params: { blogId: string 
         }
       }
     `,
-    variables: { id: blogId },
+    variables: {
+      id: blogId,
+    }
   });
 
-  // Pass the fetched data as props
-  return { props: { post } };
+  if (!post) {
+    return <div>Post not found</div>;
+  }
+
+  return (
+    <main className="change__service__page">
+      <figure className="change__service__image">
+        <Image
+          src={post.coverImage.url}
+          alt="Post Cover Image"
+          width={1440}
+          height={400}
+        />
+      </figure>
+      <h2 className="change__service__page__header">{post.title}</h2>
+      <div className="change__service__content__blog">
+        <div dangerouslySetInnerHTML={{ __html: post.content.html }} />
+      </div>
+    </main>
+  );
 }
